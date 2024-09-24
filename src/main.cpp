d8efdbe9cd218ff63a1b8ad42e5ec3ee1f20ca30
JlordS32 <jlordsty128@gmail.com>
2024-09-24 15:59:08 +1000

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,37 @@
 
 using namespace std;
 
-#include "./classes/FixedDemandDriver.h"
+#include "./classes/ChargingAllocation.h"
 #include "./classes/ChargingStation.h"
+#include "./classes/DemandGenerator.h"
 
 const int NUM_STATIONS = 12;
 
+void displayVehicleHeader();
+void displayChargingStationHeader();
+
 int main() {
-    FixedDemandDriver driver;
+    ChargingAllocation allocate;
     vector<ChargingStation> stations;
+    DemandGenerator generator;
     
-    string fileName = driver.queryFile();
-    driver.run("./assets/files/" + fileName);
-    
-    cout << "\n\n\nVEHICLE INFORMATION: " << endl;
+    string fileName = allocate.queryFile();
+    allocate.run(fileName);
+
+    displayVehicleHeader();
+    allocate.print();
+
+    displayChargingStationHeader();
+    for (int i = 0; i < NUM_STATIONS; i++) {
+        stations.push_back(ChargingStation(i));
+        stations[i].print();
+    }
+
+    return 0;
+}
+
+void displayVehicleHeader() {
+    cout << "\n\nVEHICLE INFORMATION: " << endl;
     cout << "-------------------------------------------------------------------------------------------" << endl;
     cout << "Vehicle Id" << setw(15) 
          << "Origin" << setw(20)
@@ -23,19 +41,14 @@ int main() {
          << "Capacity Range" << setw(20) 
          << "Remaining Range" << endl;
     cout << "-------------------------------------------------------------------------------------------" << endl;
-    driver.print();
+}
 
-    cout << "\n\n\nCHARGING STATION INFORMATION: " << endl;
+void displayChargingStationHeader() {
+    cout << "\n\nCHARGING STATION INFORMATION: " << endl;
     cout << "-------------------------------------------------------------------------------------------" << endl;
     cout << "Location Id" << setw(20)
          << "Location Name" << setw(25)
          << "Distance to Last City" << setw(25)
          << "Number of Chargers" << endl;
     cout << "-------------------------------------------------------------------------------------------" << endl;
-    for (int i = 0; i < NUM_STATIONS; i++) {
-        stations.push_back(ChargingStation(i));
-        stations[i].print();
-    }
-
-    return 0;
 }
